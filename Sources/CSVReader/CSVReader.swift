e71e29f2a601dd2754b1dd212bef90cb7ea4071e
Jaap Wijnen <wijnen.jaap@gmail.com>
2019-03-05 21:38:44 +0100

    add support for quoted strings

--- Sources/CSVReader/CSVReader.swift
+++ Sources/CSVReader/CSVReader.swift
@@ -4,25 +4,27 @@ import FileReader
 public class CSVReader {
     private var fileReader: FileReader
     
+    private let hasQuotes: Bool
     private let hasHeader: Bool
     public var headers: [String]?
     private(set) public var currentLine: [String]?
     
     @inlinable
-    public convenience init(fileAtPath path: String, hasHeader: Bool = true, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
+    public convenience init(fileAtPath path: String, hasHeader: Bool = true, hasQuotes: Bool = false, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
         let url = URL(fileURLWithPath: path)
-        try self.init(url: url, hasHeader: hasHeader, chunkSize: chunkSize, encoding: encoding)
+        try self.init(url: url, hasHeader: hasHeader, hasQuotes: hasQuotes, chunkSize: chunkSize, encoding: encoding)
     }
     
     @inlinable
-    public convenience init(url: URL, hasHeader: Bool = true, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
+    public convenience init(url: URL, hasHeader: Bool = true, hasQuotes: Bool = false, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
         let fileHandle = try FileHandle(forReadingFrom: url)
-        try self.init(fileHandle: fileHandle, hasHeader: hasHeader, chunkSize: chunkSize, encoding: encoding)
+        try self.init(fileHandle: fileHandle, hasHeader: hasHeader, hasQuotes: hasQuotes, chunkSize: chunkSize, encoding: encoding)
     }
 
     
-    public init(fileHandle: FileHandle, hasHeader: Bool = true, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
+    public init(fileHandle: FileHandle, hasHeader: Bool = true, hasQuotes: Bool = false, chunkSize: Int = 4096, encoding: String.Encoding = .utf8) throws {
         self.fileReader = FileReader(fileHandle: fileHandle, delimiter: "\n", chunkSize: chunkSize, encoding: encoding)
+        self.hasQuotes = hasQuotes
         self.hasHeader = hasHeader
         if self.hasHeader {
             guard let headers = self.readLine() else {
@@ -32,8 +34,9 @@ public class CSVReader {
         }
     }
     
-    public init(string: String, hasHeader: Bool = true) throws {
+    public init(string: String, hasHeader: Bool = true, hasQuotes: Bool = false) throws {
         self.fileReader = try FileReader(string: string, delimiter: "\n")
+        self.hasQuotes = hasQuotes
         self.hasHeader = hasHeader
         if self.hasHeader {
             guard let headers = self.readLine() else {
@@ -45,8 +48,36 @@ public class CSVReader {
     
     @discardableResult
     public func readLine() -> [String]? {
-        let line = self.fileReader.readLine()
-        let strings = line?.split(separator: ",", omittingEmptySubsequences: false).map { String($0) }
+        guard let line = self.fileReader.readLine() else {
+            return nil
+        }
+        
+        var strings: [String] = []
+        if hasQuotes {
+            
+            guard let lineData = line.data(using: self.fileReader.encoding) else {
+                fatalError("Could not convert string to data")
+            }
+            
+            let delim = "\",\"".data(using: self.fileReader.encoding)!
+            
+            var lowerIndex = lineData.startIndex
+            
+            while true {
+                if let range = lineData.range(of: delim, in: lowerIndex..<lineData.endIndex) {
+                    let subData = lineData.subdata(in: lowerIndex..<range.lowerBound)
+                    guard let string = String(data: subData, encoding: self.fileReader.encoding) else {
+                        fatalError("Could not convert data to string")
+                    }
+                    strings.append(string)
+                    lowerIndex = range.upperBound
+                } else {
+                    break
+                }
+            }
+        } else {
+            strings = line.split(separator: ",", omittingEmptySubsequences: false).map { String($0) }
+        }
         self.currentLine = strings
         return strings
     }
